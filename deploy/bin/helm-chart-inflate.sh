1f04e6c630eef53ecf94581e33a3c44974deb49d
Rob Watson <rob@netflux.io>
2025-04-26 12:28:33 +0200

    feat: install cert-manager

--- deploy/bin/helm-chart-inflate.sh
+++ deploy/bin/helm-chart-inflate.sh
@@ -9,6 +9,7 @@ IFS=$'\n\t'
 name="$1"
 chart="$2"
 base="$3"
+namespace="$4"
 
 mkdir -p "$base/inflated"
 rm -rf "$base/inflated/$name"
@@ -16,10 +17,10 @@ rm -rf "$base/inflated/$name"
 valuesfile="$base/values/$name.yaml"
 if [ -f $valuesfile ]; then
   echo "Inflating template with values file: $valuesfile ..."
-  helm template $name $chart --output-dir $base/inflated -f $valuesfile 
+  helm template $name $chart --output-dir $base/inflated --namespace $namespace -f $valuesfile 
 else
   echo "Inflating template with default values ..."
-  helm template $name $chart --output-dir $base/inflated
+  helm template $name $chart --output-dir $base/inflated --namespace $namespace
 fi
 
 echo "YAML entries for $base/kustomzation.yaml:"
